e676a43e7e5d3121f0fbf02e50bd532b5802ac6d
beatriz <robertabeatriz351@gmail.com>
2026-08-29 09:36:06 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Beatriz Roberta Alves dos Santos</h1>
+    <p>Linkedin: <a href="https://www.linkedin.com/in/beatriz-roberta-alves-dos-santos-3a5a8b20a/" target="_blank">Beatriz Alves</a></p>
+    <p>GitHub: <a href="https://github.com/Beatriz-Slitycat" target="_blank">Beatriz-Slitycat</a></p>
+    <h2>OBJETIVO</h2>
+    <p>
+      Graduanda em Ciência da Computação buscando oportunidade de estágio em TI para aplicar conhecimentos em 
+lógica de programação, Python/C e suporte técnico, focando na resolução de problemas e eficiência 
+operacional
+    </p>
+    <h2>FORMAÇÃO ACADEMICA</h2>
+    <p>Bacharelado em Ciência da Computação – IFMG Campus Ibirité </p>
+    <p>Período atual: 2º Período.</p>
+    <h2>HABILIDADES TÉCNICAS</h2>
+    <p>Linguagens de Programação: Python, C básico e JavaScript básico.</p>
+    <p>Linguagens de Marcação: HTML5 E CSS.</p>
+    <p>Produtividade e Design: Pacote Office (Excel, Word), Canva, CorelDRAW. </p>
+    <p>Soft skills: Foco em Aprendizagem Ativo,  Pensamento Analítico, Otimização de Processos e resolução de 
+problemas do cliente.</p>
+    <p>Possuo perfil observador, Resiliente, facilidade de adaptação e Trabalho em Equipe.</p>
+    <p>Idiomas: Inglês Básico.</p>
+    <h2>EXPERIÊNCIA PROFISSIONAL</h2>
+    <p>Elis Moda Mão | Assistente de E-commerce  Junho de 2023 – Maio de 2025</p>
+    <p>Gestão de atendimento ao cliente via plataformas digitais e suporte especializado,com 500 atendimentos 
+mensais. 
+Utilização de ferramentas de design (Canva/CorelDRAW) para comunicação visual de produtos e gerenciamento 
+de estratégia de marketing. 
+Negociação com fornecedores, elaboração de orçamentos e organização de inventário.</p>
+    <p>Freelancer – Atendimento e Suporte (LAN House)</p>
+    <p>Suporte técnico a 10 usuários, resolução de problemas em software e gestão de sistemas de PDV e 
+implementação de sistemas com GLPI,melhorando o tempo de atendimento em 30%.</p>
+    </>
+  )
+}
+
+export default App
